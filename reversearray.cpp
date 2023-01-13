84f36aee01877d9ed9064f111472156f4966ebbe
Prabhat Mishra <103622186+prabhat706@users.noreply.github.com>
2023-01-13 14:09:14 +0530

    REVERSE ARRAY
    ###

---
+++ reversearray.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseArray(int size, int arr[])
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArray(int size, int arr[])
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the value of size" << endl;
+    cin >> size;
+
+    int arr[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+
+    reverseArray(size, arr);
+    printArray(size, arr);
+}
