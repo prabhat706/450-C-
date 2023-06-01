9906d2d3094944e6e7a3408c9eb07a52f8480893
Prabhat Mishra <103622186+prabhat706@users.noreply.github.com>
2023-06-01 10:42:57 +0530

    Add files via upload
    DELETE MIDDLE FROM STACK CPP

---
+++ deletemiddlefromstack.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void deleteMiddleElementFormStack(int element, int *arr, int size)
+{
+    stack<int> s;
+    if (s.empty())
+    {
+        cout << "Stack is empty" << endl;
+    }
+}
