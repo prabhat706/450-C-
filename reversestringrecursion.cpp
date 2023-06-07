9f890d70a87684a5866a8831e50f71d7a3eca72b
Prabhat Mishra <103622186+prabhat706@users.noreply.github.com>
2023-06-07 13:12:38 +0530

    Add files via upload
    REVERSE STRING USING RECURSION CPP

---
+++ reversestringrecursion.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseString(string &str, int i, int j)
+{
+    if (i > j)
+    {
+        return;
+    }
+    swap(str[i], str[j]);
+    i++;
+    j--;
+
+    reverseString(str, i, j);
+}
+
+int main()
+{
+    string str = "TAHBARP";
+
+    reverseString(str, 0, str.length() - 1);
+    cout << str << " ";
+}
